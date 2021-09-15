dcb3fd5c2e762cc3e3a38212b574511eb25b7790
JulieVedel <juli76k0@edu.easj.dk>
2021-09-15 16:58:20 +0200

    Exercises

--- src/com/company/Exercise4.java
+++ src/com/company/Exercise4.java
@@ -1,11 +1,23 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Exercise4 {
     public static void main(String[] args) {
-
+        sieve(30);
     }
 
-    public static boolean sieve(int n) {
+    public static void sieve(int n) {
+        int[] numbers = new int [n];
+        int p = 2;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i;
 
+            if (numbers[i] % p != 0) {
+                int[] array = 
+            }
+        }
+
+        System.out.println(Arrays.toString(numbers));
     }
 }
--- src/com/company/Exercise5.java
+++ src/com/company/Exercise5.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class Exercise5 {
+    public static void main(String[] args){
+        int[] array = {3, 6, 9, 12, 30};
+        System.out.println(areFactors(3, array));
+    }
+
+    public static boolean areFactors(int n, int[] array) {
+        boolean factor = true;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % n != 0) {
+                factor = false;
+            }
+        }
+        return factor;
+    }
 }
